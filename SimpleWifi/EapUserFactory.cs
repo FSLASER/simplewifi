5a34c8ff226c8de44022c80df1f24e26fcca220a
Ellie Boyle <ellie@fullspectrumlaser.com>
2018-09-24 10:48:46 -0700

    Run strings through EncodeForXML before saving to XML Profiles.

--- SimpleWifi/EapUserFactory.cs
+++ SimpleWifi/EapUserFactory.cs
@@ -8,62 +8,74 @@ using System.Text;
 
 namespace SimpleWifi
 {
-	internal static class EapUserFactory
-	{
-		/// <summary>
-		/// Generates the EAP user XML
-		/// </summary>
-		internal static string Generate(Dot11CipherAlgorithm cipher, string username, string password, string domain)
-		{
-			#warning Robin: Probably not properly implemented, only supports WPA- and WPA-2 Enterprise with PEAP-MSCHAPv2
+    internal static class EapUserFactory
+    {
+        /// <summary>
+        /// Generates the EAP user XML
+        /// </summary>
+        internal static string Generate(Dot11CipherAlgorithm cipher, string username, string password, string domain)
+        {
+#warning Robin: Probably not properly implemented, only supports WPA- and WPA-2 Enterprise with PEAP-MSCHAPv2
 
-			string profile = string.Empty;
-			string template = string.Empty;
-			
-			switch (cipher)
-			{
-				case Dot11CipherAlgorithm.CCMP: // WPA-2
-				case Dot11CipherAlgorithm.TKIP: // WPA
-					template = GetTemplate("PEAP-MS-CHAPv2");
+            string profile = string.Empty;
+            string template = string.Empty;
 
-					profile = string.Format(template, username, FixPass(password), domain);
-					break;
-				default:
-					throw new NotImplementedException("Profile for selected cipher algorithm is not implemented");
-			}
+            switch (cipher)
+            {
+                case Dot11CipherAlgorithm.CCMP: // WPA-2
+                case Dot11CipherAlgorithm.TKIP: // WPA
+                    template = GetTemplate("PEAP-MS-CHAPv2");
 
-			return profile;
-		}
+                    profile = string.Format(
+                        template,
+                        EncodeForXML(username, true),
+                        EncodeForXML(password, true),
+                        EncodeForXML(domain, true));
+                    break;
+                default:
+                    throw new NotImplementedException("Profile for selected cipher algorithm is not implemented");
+            }
 
-		/// <summary>
-		/// Fetches the template for an EAP user
-		/// </summary>
-		private static string GetTemplate(string name)
-		{
-			string resourceName = string.Format("SimpleWifi.EapUserXML.{0}.xml", name);
+            return profile;
+        }
 
-			using (StreamReader reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)))
-			{
-				return reader.ReadToEnd();
-			}
-		}
+        /// <summary>
+        /// Fetches the template for an EAP user
+        /// </summary>
+        private static string GetTemplate(string name)
+        {
+            string resourceName = string.Format("SimpleWifi.EapUserXML.{0}.xml", name);
 
-		private static string FixPass(string pass)
-		{
-			pass = EncodeToBase64(pass);
-			pass = pass.Replace("&", "&#038;");
-			pass = pass.Replace("<", "&#060;");
-			pass = pass.Replace(">", "&#062;");
+            using (StreamReader reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)))
+            {
+                return reader.ReadToEnd();
+            }
+        }
 
-			return pass;
-		}
+        internal static string EncodeForXML(string toxml, bool base64 = false)
+        {
+            if (!string.IsNullOrEmpty(toxml))
+            {
+                if (base64)
+                {
+                    toxml = EncodeToBase64(toxml);
+                }
 
+                toxml = toxml.Replace("&", "&#038;");
+                toxml = toxml.Replace("<", "&#060;");
+                toxml = toxml.Replace(">", "&#062;");
+                toxml = toxml.Replace("'", "&apos;");
+                toxml = toxml.Replace("\"", "&quot;");
+            }
 
-		private static string EncodeToBase64(string toEncode)
-		{
-			byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
-			string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
-			return returnValue;
-		}
-	}
+            return toxml;
+        }
+
+        private static string EncodeToBase64(string toEncode)
+        {
+            byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+    }
 }
--- SimpleWifi/ProfileFactory.cs
+++ SimpleWifi/ProfileFactory.cs
@@ -9,92 +9,93 @@ using System.Text;
 
 namespace SimpleWifi
 {
-	internal static class ProfileFactory
-	{
-		/// <summary>
-		/// Generates the profile XML for the access point and password
-		/// </summary>
-		internal static string Generate(WlanAvailableNetwork network, string password)
-		{
-			string profile	= string.Empty;
-			string template = string.Empty;
-			string name		= Encoding.ASCII.GetString(network.dot11Ssid.SSID, 0, (int)network.dot11Ssid.SSIDLength);
-			string hex		= GetHexString(network.dot11Ssid.SSID);	
+    internal static class ProfileFactory
+    {
+        /// <summary>
+        /// Generates the profile XML for the access point and password
+        /// </summary>
+        internal static string Generate(WlanAvailableNetwork network, string password)
+        {
+            string profile = string.Empty;
+            string template = string.Empty;
+            string name = EapUserFactory.EncodeForXML(Encoding.ASCII.GetString(network.dot11Ssid.SSID, 0, (int)network.dot11Ssid.SSIDLength));
+            string hex = GetHexString(network.dot11Ssid.SSID);
+            string encodedPassword = EapUserFactory.EncodeForXML(password);
 
-			var authAlgo = network.dot11DefaultAuthAlgorithm;
+            var authAlgo = network.dot11DefaultAuthAlgorithm;
 
-			switch (network.dot11DefaultCipherAlgorithm)
-			{
-				case Dot11CipherAlgorithm.None:
-					template = GetTemplate("OPEN");					
-					profile = string.Format(template, name, hex);
-					break;
-				case Dot11CipherAlgorithm.WEP:
-					template = GetTemplate("WEP");					
-					profile = string.Format(template, name, hex, password);
-					break;
-				case Dot11CipherAlgorithm.CCMP:
-					if (authAlgo == Dot11AuthAlgorithm.RSNA)
-					{
-						template = GetTemplate("WPA2-Enterprise-PEAP-MSCHAPv2");
-						profile = string.Format(template, name);
-					}
-					else // PSK
-					{
-						template = GetTemplate("WPA2-PSK");
-						profile = string.Format(template, name, password);
-					}
-					break;
-				case Dot11CipherAlgorithm.TKIP:
-					#warning Robin: Not sure WPA uses RSNA
-					if (authAlgo == Dot11AuthAlgorithm.RSNA)
-					{
-						template = GetTemplate("WPA-Enterprise-PEAP-MSCHAPv2");
-						profile = string.Format(template, name);
-					}
-					else // PSK
-					{
-						template = GetTemplate("WPA-PSK");
-						profile = string.Format(template, name, password);
-					}
+            switch (network.dot11DefaultCipherAlgorithm)
+            {
+                case Dot11CipherAlgorithm.None:
+                    template = GetTemplate("OPEN");
+                    profile = string.Format(template, name, hex);
+                    break;
+                case Dot11CipherAlgorithm.WEP:
+                    template = GetTemplate("WEP");
+                    profile = string.Format(template, name, hex, encodedPassword);
+                    break;
+                case Dot11CipherAlgorithm.CCMP:
+                    if (authAlgo == Dot11AuthAlgorithm.RSNA)
+                    {
+                        template = GetTemplate("WPA2-Enterprise-PEAP-MSCHAPv2");
+                        profile = string.Format(template, name);
+                    }
+                    else // PSK
+                    {
+                        template = GetTemplate("WPA2-PSK");
+                        profile = string.Format(template, name, encodedPassword);
+                    }
+                    break;
+                case Dot11CipherAlgorithm.TKIP:
+#warning Robin: Not sure WPA uses RSNA
+                    if (authAlgo == Dot11AuthAlgorithm.RSNA)
+                    {
+                        template = GetTemplate("WPA-Enterprise-PEAP-MSCHAPv2");
+                        profile = string.Format(template, name);
+                    }
+                    else // PSK
+                    {
+                        template = GetTemplate("WPA-PSK");
+                        profile = string.Format(template, name, encodedPassword);
+                    }
 
-					break;			
-				default:
-					throw new NotImplementedException("Profile for selected cipher algorithm is not implemented");
-			}					
+                    break;
+                default:
+                    throw new NotImplementedException("Profile for selected cipher algorithm is not implemented");
+            }
 
-			return profile;
-		}
+            return profile;
+        }
 
-		/// <summary>
-		/// Fetches the template for an wireless connection profile.
-		/// </summary>
-		private static string GetTemplate(string name)
-		{
-			string resourceName = string.Format("SimpleWifi.ProfileXML.{0}.xml", name);
+        /// <summary>
+        /// Fetches the template for an wireless connection profile.
+        /// </summary>
+        private static string GetTemplate(string name)
+        {
+            string resourceName = string.Format("SimpleWifi.ProfileXML.{0}.xml", name);
 
-			using (StreamReader reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)))
-			{
-				return reader.ReadToEnd();
-			}
-		}
+            using (StreamReader reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)))
+            {
+                return reader.ReadToEnd();
+            }
+        }
 
-		/// <summary>
-		/// Converts an byte array into the hex representation, ex: [255, 255] -> "FFFF"
-		/// </summary>
-		private static string GetHexString(byte[] ba)
-		{
-			StringBuilder sb = new StringBuilder(ba.Length * 2);
+        /// <summary>
+        /// Converts an byte array into the hex representation, ex: [255, 255] -> "FFFF"
+        /// </summary>
+        private static string GetHexString(byte[] ba)
+        {
+            StringBuilder sb = new StringBuilder(ba.Length * 2);
 
-			foreach (byte b in ba)
-			{
-				if (b == 0)
-					break;
+            foreach (byte b in ba)
+            {
+                if (b == 0)
+                    break;
 
-				sb.AppendFormat("{0:x2}", b);
-			}
-			
-			return sb.ToString();
-		}
-	}
+                sb.AppendFormat("{0:x2}", b);
+            }
+
+            return sb.ToString();
+        }
+    }
 }
